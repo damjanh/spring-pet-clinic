f44632d9d3e8a8363fd26d97e0259a8e7edab423
damjanh <damjan.hudobreznik@gmail.com>
2020-09-21 09:47:32 +0200

    Enhance Owners with contact info and add pets on startup. Closes #53.

--- pet-clinic-web/src/main/java/si/damjanh/springpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/si/damjanh/springpetclinic/bootstrap/DataLoader.java
@@ -3,11 +3,14 @@ package si.damjanh.springpetclinic.bootstrap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import si.damjanh.springpetclinic.model.Owner;
+import si.damjanh.springpetclinic.model.Pet;
 import si.damjanh.springpetclinic.model.PetType;
 import si.damjanh.springpetclinic.model.Vet;
 import si.damjanh.springpetclinic.services.OwnerService;
 import si.damjanh.springpetclinic.services.PetTypeService;
 import si.damjanh.springpetclinic.services.VetService;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -37,14 +40,35 @@ public class DataLoader implements CommandLineRunner {
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
+        owner1.setAddress("123 Lafayette Street");
+        owner1.setCity("Boston");
+        owner1.setTelephone("21057129509");
+
+        Pet michaelsPet = new Pet();
+        michaelsPet.setPetType(savedDogType);
+        michaelsPet.setName("Fluffy");
+        michaelsPet.setBirthDate(LocalDate.now());
+        owner1.getPets().add(michaelsPet);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Fiona");
         owner2.setLastName("Glendale");
+        owner2.setAddress("53 George Street");
+        owner2.setCity("Austin");
+        owner2.setTelephone("2935792375");
+
+        Pet fionasPet = new Pet();
+        fionasPet.setPetType(savedCatType);
+        fionasPet.setName("Nightshade");
+        fionasPet.setBirthDate(LocalDate.now());
+        owner2.getPets().add(fionasPet);
 
         Owner owner3 = new Owner();
         owner3.setFirstName("Sam");
         owner3.setLastName("Axe");
+        owner3.setAddress("343C Fifth Street");
+        owner3.setCity("New York");
+        owner3.setTelephone("6943063454");
 
         ownerService.save(owner1);
         ownerService.save(owner2);
