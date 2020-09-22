8a21903031ef95b2811f7419e2d004fe4529aba4
damjanh <damjan.hudobreznik@gmail.com>
2020-09-22 08:02:54 +0200

    Create Spring Data JPA service for owners. Closes #60.

--- pet-clinic-data/src/main/java/si/damjanh/springpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/si/damjanh/springpetclinic/repositories/OwnerRepository.java
@@ -1,8 +1,9 @@
 package si.damjanh.springpetclinic.repositories;
 
 import org.springframework.data.repository.CrudRepository;
-
-import java.security.acl.Owner;
+import si.damjanh.springpetclinic.model.Owner;
 
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+
+    Owner findByLastName(String lastName);
 }
